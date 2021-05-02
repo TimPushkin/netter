cdbdd338f47cee65b354bc66ab9ed29d82974033
AShlegel <st087000@student.spbu.ru>
2021-05-02 14:43:54 +0300

    Added parameters according to signature

--- src/main/kotlin/ru/spbu/netter/view/MainView.kt
+++ src/main/kotlin/ru/spbu/netter/view/MainView.kt
@@ -3,19 +3,22 @@ package ru.spbu.netter.view
 import ru.spbu.netter.controller.LayoutMethod
 import ru.spbu.netter.controller.CircularLayout
 import ru.spbu.netter.controller.SmartLayout
+import ru.spbu.netter.model.Graph
+import ru.spbu.netter.model.UndirectedGraph
 import tornadofx.*
 
 class MainView : View() {
     private val defaultLayout: LayoutMethod by inject<CircularLayout>()
     private val smartLayout: LayoutMethod by inject<SmartLayout>()
+    private val graph: Graph = UndirectedGraph()
 
     override val root = vbox {
         button("Layout").setOnAction {
-            smartLayout.layout()
+            smartLayout.layout(graph)
         }
     }
 
     init {
-        defaultLayout.layout()
+        defaultLayout.layout(graph)
     }
 }
