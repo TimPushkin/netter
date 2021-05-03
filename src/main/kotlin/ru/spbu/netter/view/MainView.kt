1d1e95bbeca2b6f86fd35917a0a1ba09e10d1321
TimPushkin <pushkin.td@gmail.com>
2021-05-03 21:40:06 +0300

    Fix errors caused by unexpected user behavior

--- src/main/kotlin/ru/spbu/netter/view/MainView.kt
+++ src/main/kotlin/ru/spbu/netter/view/MainView.kt
@@ -1,5 +1,7 @@
 package ru.spbu.netter.view
 
+import javafx.beans.property.ObjectProperty
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.scene.control.Alert
 import javafx.scene.control.TextField
@@ -11,7 +13,7 @@ import java.io.File
 
 class MainView : View("Netter") {
     private lateinit var graphView: GraphView
-    private val availableFileIOHandlerNames = listOf("Plain text", "Neo4J", "SQLite")
+    private val fileIOHandlerNames = listOf("Plain text", "Neo4J", "SQLite")
     private val defaultLayout: LayoutMethod by inject<CircularLayout>()
     private val smartLayout: LayoutMethod by inject<SmartLayout>()
 
@@ -21,45 +23,48 @@ class MainView : View("Netter") {
         left = vbox {
             button("Import network").setOnAction {
                 val graph = UndirectedGraph()
-
                 val (fileIOHandler, file) = getFile()
-                fileIOHandler.importNetwork(graph, file)
 
-                graphView = GraphView(graph).apply {
-                    applyLayout(defaultLayout.layOut(graph, repulsion = getRepulsion()))
+                if (fileIOHandler != null && file != null) {
+                    fileIOHandler.importNetwork(graph, file)
+
+                    getRepulsion()?.let {
+                        graphView = GraphView(graph).apply {
+                            applyLayout(defaultLayout.layOut(graph, repulsion = it))
+                        }
+                        center = graphView
+                    }
                 }
-
-                center = graphView
             }
 
             button("Export network").setOnAction {
                 if (this@MainView::graphView.isInitialized) {
                     val (fileIOHandler, file) = getFile()
-                    fileIOHandler.exportNetwork(graphView.graph, file)
+                    if (fileIOHandler != null && file != null) fileIOHandler.exportNetwork(graphView.graph, file)
                 } else alert(
                     Alert.AlertType.INFORMATION,
                     "Nothing to export",
-                    "To export a network you need to import one first"
+                    "To export a network you need to import one first",
                 )
             }
 
             button("Apply smart layout").setOnAction {
                 if (this@MainView::graphView.isInitialized) {
-                    graphView.applyLayout(smartLayout.layOut(graphView.graph, repulsion = getRepulsion()))
+                    getRepulsion()?.let { graphView.applyLayout(smartLayout.layOut(graphView.graph, repulsion = it)) }
                 } else alert(
                     Alert.AlertType.INFORMATION,
                     "Nothing to lay out",
-                    "To lay out a network you need to import one first"
+                    "To lay out a network you need to import one first",
                 )
             }
 
             button("Apply default layout").setOnAction {
                 if (this@MainView::graphView.isInitialized) {
-                    graphView.applyLayout(defaultLayout.layOut(graphView.graph, repulsion = getRepulsion()))
+                    getRepulsion()?.let { graphView.applyLayout(defaultLayout.layOut(graphView.graph, repulsion = it)) }
                 } else alert(
                     Alert.AlertType.INFORMATION,
                     "Nothing to lay out",
-                    "To lay out a network you need to import one first"
+                    "To lay out a network you need to import one first",
                 )
             }
 
@@ -69,7 +74,7 @@ class MainView : View("Netter") {
                 } else alert(
                     Alert.AlertType.INFORMATION,
                     "Nothing to inspect",
-                    "To inspect a network you need to import one first"
+                    "To inspect a network you need to import one first",
                 )
             }
 
@@ -79,7 +84,7 @@ class MainView : View("Netter") {
                 } else alert(
                     Alert.AlertType.INFORMATION,
                     "Nothing to inspect",
-                    "To inspect a network you need to import one first"
+                    "To inspect a network you need to import one first",
                 )
             }
         }
@@ -87,67 +92,72 @@ class MainView : View("Netter") {
         center = label("Import a network to be shown here")
     }
 
-    private fun getFile(): Pair<FileIOHandler, File> {
+    private fun getFile(): Pair<FileIOHandler?, File?> {
         val fileIOHandlerName = TextField()
-        val files = mutableListOf<File>()
+        val file = SimpleObjectProperty<File?>(null)
 
         find<FileInputForm>(
             mapOf(
-                FileInputForm::availableFileIOHandlerNames to availableFileIOHandlerNames,
+                FileInputForm::fileIOHandlerNames to fileIOHandlerNames,
                 FileInputForm::fileIOHandlerName to fileIOHandlerName,
-                FileInputForm::files to files,
+                FileInputForm::file to file,
             )
         ).openModal(block = true, resizable = false)
 
-        val fileIOHandler = when (fileIOHandlerName.text) {
-            availableFileIOHandlerNames[0] -> find<TxtIOHandler>()
-            availableFileIOHandlerNames[1] -> TODO("Neo4J")
-            availableFileIOHandlerNames[2] -> TODO("SQLite")
-            else -> throw IllegalStateException("Unknown file IO handler selected: ${fileIOHandlerName.text}")
-        }
-
-        return Pair(fileIOHandler, files[0])
+        return if (file.value != null && fileIOHandlerName.text in fileIOHandlerNames) {
+            val fileIOHandler = when (fileIOHandlerName.text) {
+                fileIOHandlerNames[0] -> find<TxtIOHandler>()
+                fileIOHandlerNames[1] -> TODO("Neo4J")
+                fileIOHandlerNames[2] -> TODO("SQLite")
+                else -> throw IllegalStateException("Unknown file IO handler selected: ${fileIOHandlerName.text}")
+            }
+            Pair(fileIOHandler, file.value)
+        } else Pair(null, null)
     }
 
-    private fun getRepulsion(): Double {
+    private fun getRepulsion(): Double? {
         with(TextField()) {
             find<RepulsionInputForm>(mapOf(RepulsionInputForm::repulsion to this)).openModal(
                 block = true,
-                resizable = false
+                resizable = false,
             )
-            return text.toDouble()
+            return text.toDoubleOrNull()
         }
     }
 }
 
-class FileInputForm : Fragment("File path input") {
-    val availableFileIOHandlerNames: List<String> by param()
+class FileInputForm : Fragment("File input") {
+    val fileIOHandlerNames: List<String> by param()
     val fileIOHandlerName: TextField by param()
-    val files: MutableList<File> by param()
+    val file: ObjectProperty<File?> by param()
 
     override val root = form {
-        val selectedFileIOHandlerName = SimpleStringProperty(availableFileIOHandlerNames[0])
+        val selectedFileIOHandlerName = SimpleStringProperty(fileIOHandlerNames[0])
 
         fieldset {
             field("Select file type:") {
-                combobox(selectedFileIOHandlerName, availableFileIOHandlerNames)
+                combobox(selectedFileIOHandlerName, fileIOHandlerNames)
             }
 
-            button("Choose file...").setOnAction {
-                files.addAll(
-                    chooseFile(
-                        "Choose file to import network from...",
-                        emptyArray(),
-                        mode = FileChooserMode.Single
-                    )
-                )
+            button("Select a file...").setOnAction {
+                file.value = chooseFile(
+                    "Choose a file to import a network from...",
+                    emptyArray(),
+                    mode = FileChooserMode.Single,
+                ).firstOrNull()
             }
         }
 
         buttonbar {
             button("OK").setOnAction {
-                fileIOHandlerName.text = selectedFileIOHandlerName.value
-                close()
+                if (file.value != null) {
+                    fileIOHandlerName.text = selectedFileIOHandlerName.value
+                    close()
+                } else alert(
+                    Alert.AlertType.INFORMATION,
+                    "No file selected",
+                    "You need to select a file to import a network from",
+                )
             }
         }
     }
@@ -167,8 +177,10 @@ class RepulsionInputForm : Fragment("Repulsion input") {
 
         buttonbar {
             button("OK").setOnAction {
-                repulsion.text = enteredRepulsion.text
-                close()
+                if (enteredRepulsion.text.isDouble()) {
+                    repulsion.text = enteredRepulsion.text
+                    close()
+                } else alert(Alert.AlertType.INFORMATION, "Wrong repulsion input", "Repulsion must be a decimal number")
             }
         }
     }
