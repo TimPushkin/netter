a5498b2ed7dd4225426ea2b72caa0ee1a52c2ad0
TimPushkin <pushkin.td@gmail.com>
2021-05-04 21:56:21 +0300

    Make NavigationSpace's content appear at its center

--- src/main/kotlin/ru/spbu/netter/view/NavigationSpace.kt
+++ src/main/kotlin/ru/spbu/netter/view/NavigationSpace.kt
@@ -1,5 +1,6 @@
 package ru.spbu.netter.view
 
+import javafx.beans.binding.Bindings
 import ru.spbu.netter.controller.NetworkNavigationHandler
 import ru.spbu.netter.controller.NetworkNavigator
 import tornadofx.*
@@ -8,17 +9,31 @@ import tornadofx.*
 class NavigationSpace : View() {
     private val navigator: NetworkNavigationHandler = find<NetworkNavigator>()
 
-    override val root = pane {
-        label("Import a network to be displayed here")
-    }.apply {
+    override val root = pane().apply {
         setOnMouseDragged { it?.let { navigator.handleMouseDragged(it) } }
         setOnScroll { it?.let { navigator.handleScroll(it) } }
     }
 
-    fun replaceNetwork(graph: GraphView) {
-        root.apply {
-            children.clear()
-            add(graph)
+    init {
+        root += label("Import a network to be displayed here").apply {
+            translateXProperty().bind(
+                Bindings.createDoubleBinding({ calculateRootCenterX() - width / 2 }, root.widthProperty())
+            )
+            translateYProperty().bind(
+                Bindings.createDoubleBinding({ calculateRootCenterY() - height / 2 }, root.heightProperty())
+            )
         }
     }
+
+    fun replaceNetwork(graph: GraphView) {
+        root.children.clear()
+        root += graph.apply {
+            translateX = calculateRootCenterX()
+            translateY = calculateRootCenterY()
+        }
+    }
+
+    private fun calculateRootCenterX() = root.width / 2
+
+    private fun calculateRootCenterY() = root.height / 2
 }
