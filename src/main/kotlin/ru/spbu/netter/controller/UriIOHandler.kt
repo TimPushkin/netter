7c39d2e3865a877a5064a3455de07d2bf76e6c4b
AShlegel <st087000@student.spbu.ru>
2021-05-07 18:38:20 +0300

    Add Neo4jIOHandler

---
+++ src/main/kotlin/ru/spbu/netter/controller/UriIOHandler.kt
@@ -0,0 +1,16 @@
+package ru.spbu.netter.controller
+
+import ru.spbu.netter.model.Network
+
+
+interface UriIOHandler {
+    companion object {
+        internal const val MIN_NODE_ID = 0
+        internal const val MIN_COMMUNITY = 0
+        internal const val MIN_CENTRALITY = 0.0
+    }
+
+    fun importNetwork(network: Network, uri: String, user: String, password: String)
+
+    fun exportNetwork(network: Network, uri: String, user: String, password: String)
+}
