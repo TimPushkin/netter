e0401630c74f4e234a4d16be124d5e351baa2351
TimPushkin <pushkin.td@gmail.com>
2021-05-07 22:34:24 +0300

    Add icon

--- src/main/kotlin/ru/spbu/netter/MainApp.kt
+++ src/main/kotlin/ru/spbu/netter/MainApp.kt
@@ -1,7 +1,8 @@
 package ru.spbu.netter
 
+import javafx.scene.image.Image
 import ru.spbu.netter.view.MainView
 import tornadofx.*
 
 
-class MainApp : App(MainView::class)
+class MainApp : App(Image("file:src/main/resources/icon.png"), MainView::class)
