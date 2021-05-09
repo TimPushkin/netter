22c37bcf1ffb5dbd82309f9c9050e65481859fc5
AShlegel <st087000@student.spbu.ru>
2021-05-09 21:00:47 +0300

    Handle exceptions

--- src/main/kotlin/ru/spbu/netter/controller/io/Neo4jIOHandler.kt
+++ src/main/kotlin/ru/spbu/netter/controller/io/Neo4jIOHandler.kt
@@ -1,79 +1,148 @@
 package ru.spbu.netter.controller.io
 
 import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.Result
+import org.neo4j.driver.exceptions.*
 import ru.spbu.netter.model.Network
 import tornadofx.Controller
-import java.io.IOException
+import java.io.Closeable
 import kotlin.math.max
 
 
-class Neo4jIOHandler : Controller(), UriIOHandler {
+class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
+    private lateinit var driver: Driver
 
     override fun importNetwork(network: Network, uri: String, username: String, password: String) {
-        val driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
+        openDriver(uri, username, password)
         val session = driver.session()
 
-        session.readTransaction { tx ->
-            try {
-                val nodes = tx.run(
-                    "MATCH (n:Node) " + "RETURN n.id AS id, n.community AS community, n.centrality AS centrality"
-                )
-                parseNode(network, nodes)
+        var nodes: Result
+        var links: Result
 
-                val links = tx.run(
-                    "MATCH (n1)-->(n2) " + "RETURN n1.id AS id1, n2.id AS id2"
-                )
-                parseLink(network, links)
-            } catch (ex: Exception) {
-                //tx.rollback()
-                throw IOException("Network cannot be read: ${ex.localizedMessage}")
+        try {
+            session.readTransaction { tx ->
+                try {
+                    nodes =
+                        tx.run(
+                            "MATCH (n:Node) " +
+                                    "RETURN n.id AS id, n.community AS community, n.centrality AS centrality"
+                        )
+                    parseNode(network, nodes)
+                } catch (exception: Exception) {
+                    throw when (exception) {
+                        is HandledIOException -> exception
+                        else -> {
+                            tx.rollback()
+                            HandledIOException("Unable to read nodes")
+                        }
+                    }
+                }
             }
-        }
+            session.readTransaction { tx ->
+                try {
+                    links =
+                        tx.run(
+                            "MATCH (n1)-->(n2) " +
+                                    "RETURN n1.id AS id1, n2.id AS id2"
+                        )
+                    parseLink(network, links)
+                } catch (exception: Exception) {
+                    throw when (exception) {
+                        is HandledIOException -> exception
+                        else -> {
+                            tx.rollback()
+                            HandledIOException("Unable to read links")
+                        }
+                    }
+                }
+            }
+        } catch (exception: Exception) {
+            throw when (exception) {
+                is HandledIOException -> exception
+                is AuthenticationException -> HandledIOException("Wrong username or password")
+                is ClientException -> HandledIOException("Make sure you are trying to connect to the bolt:// URI scheme")
+                else -> HandledIOException("Server error: ${exception.localizedMessage}", exception)
+            }
 
-        session.close()
-        driver.close()
+        }
     }
 
     override fun exportNetwork(network: Network, uri: String, username: String, password: String) {
-        val driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
+        openDriver(uri, username, password)
         val session = driver.session()
 
-        session.writeTransaction { tx ->
-            try {
-                tx.run("MATCH (n) DETACH DELETE n ")
+        try {
+            session.writeTransaction { tx ->
+                try {
+                    tx.run("MATCH (n) DETACH DELETE n ")
+                } catch (exception: Exception) {
+                    tx.rollback()
+                    throw HandledIOException("Unable to clean database")
+                }
 
                 for (entry in network.nodes) with(entry.value) {
-                    tx.run(
-                        "CREATE (n:Node{id:\$id, community:\$community, centrality:\$centrality})",
-                        mutableMapOf(
-                            "id" to id,
-                            "community" to community,
-                            "centrality" to centrality
-                        ) as Map<String, Any>?
-                    )
+                    try {
+                        tx.run(
+                            "CREATE (n:Node{id:\$id, community:\$community, centrality:\$centrality})",
+                            mutableMapOf(
+                                "id" to id,
+                                "community" to community,
+                                "centrality" to centrality
+                            ) as Map<String, Any>?
+                        )
+                    } catch (exception: Exception) {
+                        tx.rollback()
+                        throw HandledIOException("Unable to record nodes")
+                    }
                 }
 
                 for (link in network.links) with(link) {
-                    tx.run(
-                        "MATCH (n1:Node{id:\$id1})  " +
-                                "MATCH (n2:Node{id:\$id2}) " +
-                                "CREATE (n1)-[:LINK]->(n2)",
-                        mutableMapOf(
-                            "id1" to n1.id,
-                            "id2" to n2.id
-                        ) as Map<String, Any>?
-                    )
+                    try {
+                        tx.run(
+                            "MATCH (n1:Node{id:\$id1})  " +
+                                    "MATCH (n2:Node{id:\$id2}) " +
+                                    "CREATE (n1)-[:LINK]->(n2)",
+                            mutableMapOf(
+                                "id1" to n1.id,
+                                "id2" to n2.id
+                            ) as Map<String, Any>?
+                        )
+                    } catch (exception: Exception) {
+                        tx.rollback()
+                        throw HandledIOException("Unable to record links")
+                    }
                 }
-            } catch (ex: Exception) {
-                tx.rollback()
-                throw IOException("Network cannot be recorded: ${ex.localizedMessage}")
+
+                //throw HandledIOException("Network cannot be recorded: ${ex.localizedMessage}")
+
+            }
+        } catch (exception: Exception) {
+            throw when (exception) {
+                is HandledIOException -> exception
+                is AuthenticationException -> HandledIOException("Wrong username or password")
+                is ClientException -> HandledIOException("Make sure you are trying to connect to the bolt:// URI scheme")
+                else -> HandledIOException("Network cannot be recorded: ${exception.localizedMessage}")
             }
         }
+    }
 
-        session.close()
-        driver.close()
+    private fun openDriver(uri: String, username: String, password: String) {
+        try {
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
+        } catch (exception: Exception) {
+            throw when (exception) {
+                is java.lang.IllegalArgumentException -> HandledIOException("Wrong URI")
+                is ServiceUnavailableException -> HandledIOException(
+                    "Unable to connect, " +
+                            "ensure the database is running and that there is a working network connection to it.",
+                    exception
+                )
+                is SessionExpiredException -> HandledIOException("Session failed, try restarting the app")
+                else -> HandledIOException("Connection failed: ${exception.localizedMessage}")
+            }
+        }
     }
 
     private fun parseNode(network: Network, nodes: Result) {
@@ -83,15 +152,15 @@ class Neo4jIOHandler : Controller(), UriIOHandler {
             val parsedCentrality = node["centrality"].asDouble()
 
             if (parsedId < IOHandler.MIN_NODE_ID) {
-                handleInputError("id label must be not less than ${IOHandler.MIN_NODE_ID}")
+                throw HandledIOException("id label must be not less than ${IOHandler.MIN_NODE_ID}")
             }
 
             if (parsedCommunity < IOHandler.MIN_COMMUNITY) {
-                handleInputError("community label must be not less than ${IOHandler.MIN_COMMUNITY}")
+                throw HandledIOException("community label must be not less than ${IOHandler.MIN_COMMUNITY}")
             }
 
             if (parsedCentrality < IOHandler.MIN_CENTRALITY) {
-                handleInputError("centrality label must be not less than ${IOHandler.MIN_CENTRALITY}")
+                throw HandledIOException("centrality label must be not less than ${IOHandler.MIN_CENTRALITY}")
             }
 
             network.addNode(parsedId).apply {
@@ -109,7 +178,7 @@ class Neo4jIOHandler : Controller(), UriIOHandler {
             val parsedId2 = link["id2"].asInt()
 
             if (parsedId1 < IOHandler.MIN_NODE_ID || parsedId2 < IOHandler.MIN_NODE_ID) {
-                handleInputError("id labels must be not less than ${IOHandler.MIN_NODE_ID}")
+                throw HandledIOException("id labels must be not less than ${IOHandler.MIN_NODE_ID}")
             }
 
             addSkippedNodes(network, max(parsedId1, parsedId2))
@@ -123,7 +192,11 @@ class Neo4jIOHandler : Controller(), UriIOHandler {
         while (prevId >= IOHandler.MIN_NODE_ID && !network.nodes.containsKey(prevId)) network.addNode(prevId--)
     }
 
-    private fun handleInputError(message: String): Nothing {
-        throw IOException("Incorrect input format: $message")
+    override fun close() {
+        try {
+            driver.close()
+        } catch (exception: Neo4jException) {
+            throw HandledIOException("Unable to close connection")
+        }
     }
 }
