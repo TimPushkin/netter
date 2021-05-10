ed29a16b50f97d9935f8a2480779dc260c1c696b
matvey <st090604@student.spbu.ru>
2021-05-10 18:57:34 +0300

    Add logger, catching IllegalStateException and minor updates.

--- src/main/kotlin/ru/spbu/netter/controller/centrality/HarmonicCentralityIdentifier.kt
+++ src/main/kotlin/ru/spbu/netter/controller/centrality/HarmonicCentralityIdentifier.kt
@@ -1,5 +1,6 @@
 package ru.spbu.netter.controller.centrality
 
+import mu.KotlinLogging
 import org.jgrapht.Graph
 import org.jgrapht.alg.scoring.HarmonicCentrality
 import org.jgrapht.graph.DefaultEdge
@@ -7,25 +8,27 @@ import org.jgrapht.graph.DefaultUndirectedGraph
 import ru.spbu.netter.model.Network
 import tornadofx.Controller
 
+private val logger = KotlinLogging.logger {}
 
 class HarmonicCentralityIdentifier : Controller(), CentralityIdentifier {
 
     override fun identifyCentrality(network: Network) {
-        val buf = HarmonicCentrality(convertNetwork(network)).scores
-        network.nodes.forEach {
-            it.value.centrality = buf[it.key]!!
+
+        logger.info { "Identifying centrality..." }
+
+        val centralityValues = HarmonicCentrality(convertNetwork(network)).scores
+        network.nodes.values.forEach { node ->  
+            centralityValues[node.id]?.let { node.centrality = it }
+                ?: throw IllegalStateException("Node ${node.id} not found in the harmonic centrality calculation result")
         }
     }
 
     private fun convertNetwork(network: Network): Graph<Int, DefaultEdge> {
-        var graph = DefaultUndirectedGraph<Int, DefaultEdge>(DefaultEdge::class.java)
-        network.nodes.forEach {
-            graph.addVertex(it.key)
-        }
-        network.links.forEach {
-            graph.addEdge(it.n1.id, it.n2.id)
-        }
+        val graph = DefaultUndirectedGraph<Int, DefaultEdge>(DefaultEdge::class.java)
+
+        network.nodes.forEach { graph.addVertex(it.key) }
+        network.links.forEach { graph.addEdge(it.n1.id, it.n2.id) }
+
         return graph
     }
-
 }
