87b3492f06ba31378a7a13413c11f7d4c1c91d27
TimPushkin <pushkin.td@gmail.com>
2021-05-12 12:31:10 +0300

    Replace a part of rewritten ForceAtlas2 files with original Gephi's implementation

--- src/main/kotlin/org/gephi/layout/plugin/forceAtlas2/FaLink.kt
+++
@@ -1,8 +0,0 @@
-package org.gephi.layout.plugin.forceAtlas2
-
-import ru.spbu.netter.model.Link
-
-
-class FaLink(val link: Link) {
-    var weight = 1.0
-}
--- src/main/kotlin/ru/spbu/netter/controller/layout/ForceAtlas2Layout.kt
+++ src/main/kotlin/ru/spbu/netter/controller/layout/ForceAtlas2Layout.kt
@@ -2,7 +2,11 @@ package ru.spbu.netter.controller.layout
 
 import javafx.geometry.Point2D
 import mu.KotlinLogging
+import org.gephi.graph.api.Edge
+import org.gephi.graph.api.Graph
+import org.gephi.graph.api.Node
 import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2
+import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2LayoutData
 import ru.spbu.netter.model.Network
 import tornadofx.*
 
@@ -15,10 +19,36 @@ class ForceAtlas2Layout : Controller(), LayoutMethod {
     override fun layOut(network: Network, center: Point2D, repulsion: Double): List<Point2D> {
         logger.info { "Placing nodes in a nice shape with repulsion $repulsion..." }
 
-        val forceAtlas2Algorithm = ForceAtlas2(network)
+        val convertedNetwork = convertNetwork(network)
 
-        repeat(1000) { forceAtlas2Algorithm.start() }
+        val forceAtlas2Algorithm = ForceAtlas2().apply {
+            setGraph(convertedNetwork)
 
-        return forceAtlas2Algorithm.getResults().map { Point2D(it.x, it.y) }
+            isOutboundAttractionDistribution = false
+            isAdjustSizes = true
+            isBarnesHutOptimize = true
+            isLinLogMode = false
+            isStrongGravityMode = false
+
+            edgeWeightInfluence = 1.0
+            jitterTolerance = 1.0
+            scalingRatio = 10.0
+            gravity = 1.0
+            barnesHutTheta = 1.2
+        }
+
+        with(forceAtlas2Algorithm) {
+            initAlgo()
+            repeat(1000) { goAlgo() }
+            endAlgo()
+        }
+
+        return List(network.nodes.size) { convertedNetwork.nodes[it].run { Point2D(x, y) } }
+    }
+
+    private fun convertNetwork(network: Network): Graph {
+        val nodes = network.nodes.values.map { Node(it.id, ForceAtlas2LayoutData()) }
+        val edges = network.links.map { Edge(nodes[it.n1.id], nodes[it.n2.id]) }
+        return Graph(nodes, edges)
     }
 }
