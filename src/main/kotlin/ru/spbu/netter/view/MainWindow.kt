e3aed1104b847c88273877acaccffae045d9f270
TimPushkin <pushkin.td@gmail.com>
2021-05-14 11:20:16 +0300

    Add empty line in MainWindow class

--- src/main/kotlin/ru/spbu/netter/view/MainWindow.kt
+++ src/main/kotlin/ru/spbu/netter/view/MainWindow.kt
@@ -13,6 +13,7 @@ class MainWindow : View("Netter") {
     private val txtIOHandler: FileIOHandler by inject<TxtIOHandler>()
     private val neo4jIOHandler: UriIOHandler by inject<Neo4jIOHandler>()
     private val sqliteIHandler: FileIOHandler by inject<SQLiteIOHandler>()
+
     override val root = borderpane {
         setPrefSize(960.0, 540.0)
 
