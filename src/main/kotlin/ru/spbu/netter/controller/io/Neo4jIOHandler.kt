4849c04d735e46007edb1afcb1740a0cb80c61c4
TimPushkin <pushkin.td@gmail.com>
2021-05-17 09:11:47 +0300

    Change transaction handling in Neo4j

--- src/main/kotlin/ru/spbu/netter/controller/io/Neo4jIOHandler.kt
+++ src/main/kotlin/ru/spbu/netter/controller/io/Neo4jIOHandler.kt
@@ -1,10 +1,7 @@
 package ru.spbu.netter.controller.io
 
 import mu.KotlinLogging
-import org.neo4j.driver.AuthTokens
-import org.neo4j.driver.Driver
-import org.neo4j.driver.GraphDatabase
-import org.neo4j.driver.Result
+import org.neo4j.driver.*
 import org.neo4j.driver.exceptions.*
 import org.neo4j.driver.exceptions.value.Uncoercible
 import ru.spbu.netter.model.Network
@@ -24,33 +21,30 @@ class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
         openDriver(uri, username, password)
         val session = driver.session()
 
-        try {
-            session.readTransaction { tx ->
-                val nodes = tx.run(
-                    "MATCH (n:NODE) " +
-                            "RETURN n.id AS id, n.community AS community, n.centrality AS centrality, n.x AS x, n.y AS y"
-                )
-                parseNode(network, nodes)
-            }
-        } catch (ex: AuthenticationException) {
-            neo4jException(ex)
+        var nodes: Result? = null
+        session.handleTransaction(Session::readTransaction) { tx ->
+            nodes = tx.run(
+                "MATCH (n:NODE) " +
+                        "RETURN n.id AS id, n.community AS community, n.centrality AS centrality, n.x AS x, n.y AS y"
+            )
         }
+        nodes?.let { parseNodes(network, it) }
+            ?: throw IllegalStateException("Unable to parse nodes: nodes reading has been failed")
 
         logger.info { "Nodes were successfully read" }
 
-        try {
-            session.readTransaction { tx ->
-                val links = tx.run(
-                    "MATCH (n1:NODE)-[:LINK]->(n2:NODE) " +
-                            "RETURN n1.id AS id1, n2.id AS id2"
-                )
-                parseLink(network, links)
-            }
-        } catch (ex: AuthenticationException) {
-            neo4jException(ex)
+        var links: Result? = null
+        session.handleTransaction(Session::readTransaction) { tx ->
+            links = tx.run(
+                "MATCH (n1:NODE)-[:LINK]->(n2:NODE) " +
+                        "RETURN n1.id AS id1, n2.id AS id2"
+            )
         }
+        links?.let { parseLinks(network, it) }
+            ?: throw IllegalStateException("Unable to parse links: links reading has been failed")
 
         logger.info { "Links were successfully read" }
+
         close()
     }
 
@@ -58,29 +52,25 @@ class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
         openDriver(uri, username, password)
         val session = driver.session()
 
-        try {
-            session.writeTransaction { tx ->
-                tx.run("MATCH (n:NODE), (:NODE)-[l:LINK]->(:NODE) DETACH DELETE n, l")
-                logger.info { "Database was successfully cleaned" }
+        session.handleTransaction(Session::writeTransaction) { tx ->
+            tx.run("MATCH (n:NODE), (:NODE)-[l:LINK]->(:NODE) DETACH DELETE n, l")
+            logger.info { "Database was successfully cleaned" }
 
-                for (entry in network.nodes) with(entry.value) {
-                    tx.run(
-                        "CREATE (n:NODE{id:$id, community:$community, centrality:$centrality, x:$x, y:$y})"
-                    )
-                }
-                logger.info { "Nodes were successfully recorded" }
-
-                for (link in network.links) with(link) {
-                    tx.run(
-                        "MATCH (n1:NODE{id:${n1.id}})  " +
-                                "MATCH (n2:NODE{id:${n2.id}}) " +
-                                "CREATE (n1)-[:LINK]->(n2)"
-                    )
-                }
-                logger.info { "Links were successfully recorded" }
+            for (entry in network.nodes) with(entry.value) {
+                tx.run(
+                    "CREATE (n:NODE{id:$id, community:$community, centrality:$centrality, x:$x, y:$y})"
+                )
             }
-        } catch (ex: AuthenticationException) {
-            neo4jException(ex)
+            logger.info { "Nodes were successfully recorded" }
+
+            for (link in network.links) with(link) {
+                tx.run(
+                    "MATCH (n1:NODE{id:${n1.id}})  " +
+                            "MATCH (n2:NODE{id:${n2.id}}) " +
+                            "CREATE (n1)-[:LINK]->(n2)"
+                )
+            }
+            logger.info { "Links were successfully recorded" }
         }
 
         close()
@@ -100,7 +90,22 @@ class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
         logger.info { "Connect to the server at $uri address" }
     }
 
-    private fun parseNode(network: Network, nodes: Result) {
+    private fun <T> Session.handleTransaction(execute: Session.(TransactionWork<T>) -> T, work: TransactionWork<T>): T {
+        try {
+            return execute(work)
+        } catch (ex: AuthenticationException) {
+            logger.error(ex) { "Wrong username or password" }
+            throw HandledIOException("Wrong username or password", ex)
+        } catch (ex: ClientException) {
+            logger.error(ex) { "Make sure you are trying to connect to the bolt:// URI scheme" }
+            throw HandledIOException("Use the bolt:// URI scheme or some other expected labels", ex)
+        } catch (ex: ServiceUnavailableException) {
+            logger.error(ex) { "Unable to connect, ensure the database is running and that there is a working network connection to it" }
+            throw HandledIOException("Check your network connection", ex)
+        }
+    }
+
+    private fun parseNodes(network: Network, nodes: Result) {
         var parsedId = IOHandlerData.MIN_NODE_ID - 1
         var parsedCommunity = Node.DEFAULT_COMMUNITY
         var parsedCentrality = Node.DEFAULT_CENTRALITY
@@ -139,7 +144,7 @@ class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
         }
     }
 
-    private fun parseLink(network: Network, links: Result) {
+    private fun parseLinks(network: Network, links: Result) {
         var parsedId1 = IOHandlerData.MIN_NODE_ID - 1
         var parsedId2 = IOHandlerData.MIN_NODE_ID - 1
 
@@ -164,24 +169,6 @@ class Neo4jIOHandler : Controller(), UriIOHandler, Closeable {
     private fun addSkippedNodes(network: Network, addUntilId: Int) {
         var prevId = addUntilId - 1
         while (prevId >= IOHandlerData.MIN_NODE_ID && !network.nodes.containsKey(prevId)) network.addNode(prevId--)
-    }
-
-    private fun neo4jException(ex: Neo4jException) {
-        throw when (ex) {
-            is AuthenticationException -> {
-                logger.error(ex) { "Wrong username or password" }
-                HandledIOException("Wrong username or password", ex)
-            }
-            is ClientException -> {
-                logger.error(ex) { "Make sure you are trying to connect to the bolt:// URI scheme" }
-                HandledIOException("Use the bolt:// URI scheme or expected other labels", ex)
-            }
-            is ServiceUnavailableException -> {
-                logger.error(ex) { "Unable to connect, ensure the database is running and that there is a working network connection to it" }
-                throw HandledIOException("Check your network connection", ex)
-            }
-            else -> ex
-        }
     }
 
     override fun close() {
